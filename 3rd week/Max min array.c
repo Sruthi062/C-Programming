d234aca3cdbaab2095d8681e01f30dc0a8c37b6d
Sruthi062 <sruthialameluseshagiri@gmail.com>
2024-10-21 21:34:45 +0530

    Add files via upload

---
+++ 3rd week/Max min array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,n,num[10],max,min;
+  clrscr();
+  printf("Enter count of number:");
+  scanf("%d",& n);
+  for(i=0;i<n;i++)
+  {
+     printf("Enter number:");
+     scanf("%d",& num[i]);
+  }
+  max=min=num[0];
+  for(i=0;i<n;i++)
+  {
+    if(num[i]>max)
+      max=num[i];
+    else if(num[i]<min)
+      min=num[i];
+  }
+  printf("The maximum is:%d",max);
+  printf("\nThe minimum is:%d",min);
+  getch();
+}
